113c886445937a716b1cadac486a9a963f133236
omarsamy10 <os1764934@gmail.com>
2025-02-09 04:28:27 +0200

    json

--- tools/commandinjection.py
+++ tools/commandinjection.py
@@ -1,6 +1,28 @@
 import subprocess
 import re
-from .save_json_file import save_to_json
+import json
+
+def save_to_json(vulnerability, filename="../vulnerabilities.json"):
+    """
+    Appends a vulnerability to a JSON file.
+
+    :param vulnerability: A dictionary containing vulnerability details.
+    :param filename: The name of the JSON file to save the data.
+    """
+    try:
+        # Try to load existing data from the file
+        with open(filename, "r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        # If the file doesn't exist, initialize with an empty list
+        data = []
+
+    # Append the new vulnerability
+    data.append(vulnerability)
+
+    # Save the updated data back to the file
+    with open(filename, "w") as file:
+        json.dump(data, file, indent=4)
 
 def commandinjection(url_file):
     """
--- tools/dalfox.py
+++ tools/dalfox.py
@@ -1,8 +1,32 @@
 import subprocess
 import re
+import json
 import urllib.parse
 from urllib.parse import urlparse, parse_qs
-from .save_json_file import save_to_json
+
+def save_to_json(vulnerability, filename="../vulnerabilities.json"):
+    """
+    Appends a vulnerability to a JSON file.
+
+    :param vulnerability: A dictionary containing vulnerability details.
+    :param filename: The name of the JSON file to save the data.
+    """
+    try:
+        # Try to load existing data from the file
+        with open(filename, "r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        # If the file doesn't exist, initialize with an empty list
+        data = []
+
+    # Append the new vulnerability
+    data.append(vulnerability)
+
+    # Save the updated data back to the file
+    with open(filename, "w") as file:
+        json.dump(data, file, indent=4)
+
+
 #dalfox outputs the POC only so we decoded the payload
 
 def run_dalfox_on_url(url_file):
